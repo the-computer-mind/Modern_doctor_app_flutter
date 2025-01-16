3f400c5a54ca143235a211dadf308d4cb00c2227
Shofick <voidknown@gmail.com>
2025-01-15 23:34:24 -0800

    added permission

--- lib/main.dart
+++ lib/main.dart
@@ -49,32 +49,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // Make the app full screen
+    // Make the app full screen using this
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return MultiProvider(
       providers: [
+        //Provider to handle the api data dynamic
         ChangeNotifierProvider<ProductProvider>(
             create: (_) => ProductProvider()),
       ],
       child: MaterialApp(
-        // title: 'Flutter Demo',
+        
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
